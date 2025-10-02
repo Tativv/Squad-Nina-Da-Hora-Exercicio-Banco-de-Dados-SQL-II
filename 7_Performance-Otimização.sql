fbae5b49f0456d573abcb0caff9a0b3669ad2591
VanessaCosta91 <van.nessacic@gmail.com>
2025-10-02 14:37:13 -0300

    Adiciona script sql com exercícos de Perfomance e Otimização

---
+++ 7_Performance-Otimização.sql
@@ -0,0 +1,25 @@
+-- Indice em Orders
+CREATE INDEX idx_orders_customer_data
+ON orders(customer_id,dt_pedido)
+
+-- O index será usando quando utilizar joins, filtros e orderna pelo customer_id e dt_pedido
+
+
+-- Análise consulta mais perfomatica
+EXPLAIN QUERY PLAN
+SELECT 
+	DISTINCT c.nome AS Cliente
+FROM customers c
+JOIN orders o
+ON c.customer_id = o.customer_id;
+
+EXPLAIN QUERY PLAN
+SELECT
+	c.nome AS Cliente
+FROM customers c 
+WHERE EXISTS (SELECT *
+			FROM orders o
+			WHERE c.customer_id = o.customer_id);
+
+-- Identifique consultas que podem ser reescritas usando CTE materializada para melhorar a performance.
+	
